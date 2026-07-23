80f632281b5378060a0d561d4338b8fed022748c
mutihcd-byte <muti.hcd@mutigroup.com>
2026-07-23 14:47:37 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
    HCD ATTENDANCE KIOSK SERVICE WORKER
    ========================================================= */
 
-const APP_VERSION = "2026-07-23-03";
+const APP_VERSION = "2026-07-23-04";
 
 const CACHE_PREFIX = "hcd-attendance-";
 const CACHE_NAME =
@@ -24,8 +24,8 @@ const CACHE_NAME =
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./style.css?v=2026-07-23-03",
-  "./app.js?v=2026-07-23-03",
+  "./style.css?v=2026-07-23-04",
+  "./app.js?v=2026-07-23-04",
   "./manifest.json"
 ];
 
